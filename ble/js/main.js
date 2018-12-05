8b2b7b2c9106e52018888ec54740e69f7a25f88c
Sergey Lyubka <sergey.lyubka@cesanta.com>
2018-12-05 18:04:44 +0000

    Add wifi setup

--- ble/js/main.js
+++ ble/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 var h = preact.h;
+var app;  // App constructor sets this variable
 
 var createClass = function(obj) {
   var F = function() {
@@ -27,12 +28,18 @@ var Header = createClass({
   },
   render: function(props) {
     var self = this;
-    var title = 'Provisioning over BLE';
+    var title = 'CCM over BLE setup';
     var src = 'images/logo-512x512.png';
     var icon = h('img', {src: src, width: 32, class: 'mr-2'});
     var mkitem = function(icon, href, title, onclick) {
       return h(
-          'a', {class: 'nav-item nav-link', href: href, onClick: onclick},
+          'a', {
+            class: 'nav-item nav-link',
+            href: href,
+            onClick: function() {
+              self.setState({expanded: false});
+            }
+          },
           title, mkicon(icon));
     };
     return h(
@@ -49,8 +56,8 @@ var Header = createClass({
         h('div',
           {class: 'navbar-collapse' + (self.state.expanded ? '' : ' collapse')},
           h('ul', {class: 'navbar-nav mr-auto text-right mt-2'},
-            mkitem('fa-bluetooth', '#', title),
-            mkitem('fa-sign-out', '#login', 'Logout', function() {}))));
+            mkitem('fa-wifi', '#', 'WiFi setup'),
+            mkitem('fa-gears', '#rpc', 'Call RPC'))));
   },
 });
 
@@ -63,7 +70,9 @@ var bleRPCID = 1;
 
 var blerpc = async function(dev, method, params, cb) {
   var obj = {method: method, id: bleRPCID++};
-  if (params) obj.params = JSON.parse(params);
+  if (params) {
+    obj.params = typeof (params) == 'string' ? JSON.parse(params) : params;
+  }
   var frame = (new TextEncoder()).encode(JSON.stringify(obj));
   var srv = await dev.gatt.connect();
   var svc = await srv.getPrimaryService(rpc_uuid);
@@ -72,25 +81,32 @@ var blerpc = async function(dev, method, params, cb) {
   var b = [(n >> 24) & 255, (n >> 16) & 255, (n >> 8) & 255, n & 255];
   await tx.writeValue(new Uint8Array(b));  // Write frame length to tx
   var dx = await svc.getCharacteristic(rpc_data);
-  await dx.writeValue(frame);  // Write frame itself
+  for (var sent = 0, chunkSize = 50; sent < frame.length; sent += chunkSize) {
+    if (chunkSize > frame.length - sent) chunkSize = frame.length - sent;
+    var chunk = new DataView(frame.buffer, sent, chunkSize);
+    await dx.writeValue(chunk);
+  }
   var rx = await svc.getCharacteristic(rpc_rx);
   // console.log('rx', rx);
   for (var i = 0; i < 20; i++) {         // Try up to 20 times
     var b2 = await rx.readValue();       // Read response length
     var u8 = new Uint8Array(b2.buffer);  // big endian 32-bit number
-    var n = (u8[0] << 24) | (u8[1] << 16) | (u8[2] << 8) | u8[3];
+    var r = '', j = 0, n = (u8[0] << 24) | (u8[1] << 16) | (u8[2] << 8) | u8[3];
     if (n == 0) continue;  // If > 0, response is ready
-    var chunk = await dx.readValue();
-    return JSON.parse((new TextDecoder('utf-8')).decode(chunk));
+    while (j < n) {
+      var chunk = await dx.readValue();
+      r += (new TextDecoder('utf-8')).decode(chunk);
+      j += chunk.byteLength;
+    }
+    // console.log('r', r);
+    return JSON.parse(r);
   }
   return undefined;
 };
 
-var BLE = createClass({
+var RPC = createClass({
   init: function() {
     this.state = {
-      bt: navigator.bluetooth,
-      device: null,
       spin: false,
       selectedMethod: '',
       rpcParams: '',
@@ -98,40 +114,26 @@ var BLE = createClass({
       methods: [],
     };
   },
+  componentWillReceiveProps: function(nextProps) {
+    this.refresh();
+  },
+  refresh: async function() {
+    var self = this;
+    self.setState({spin: true, methods: []});
+    blerpc(app.state.device, 'RPC.List')
+        .then(function(f) {
+          self.setState({methods: f.result || []});
+        })
+        .catch(function(err) {
+          alert(err);
+        })
+        .then(function(res) {
+          self.setState({spin: false});
+        });
+  },
   render: function(props, state) {
     var self = this;
-    if (!self.state.bt) {
-      return h(
-          'div', {class: 'my-2'},
-          h('div', {class: 'alert alert-danger'},
-            'BLE is not supported by this platform. Please use Chrome.'));
-    }
-    var button =
-        h('button', {
-          class: 'btn btn-sm btn-info btn-block my-2',
-          disabled: state.spin,
-          onClick: async function() {
-            self.setState({spin: true});
-            state.bt
-                .requestDevice(
-                    {acceptAllDevices: true, optionalServices: [rpc_uuid]})
-                .then(async function(dev) {
-                  self.setState({device: dev});
-                  var f = await blerpc(dev, 'RPC.List', '');
-                  self.setState({methods: f.result || []});
-                })
-                .catch(function(err) {
-                  alert(err);
-                })
-                .then(function() {
-                  self.setState({spin: false});
-                });
-          },
-        },
-          mkicon(state.spin ? 'fa-refresh fa-spin' : 'fa-search'),
-          'Choose device');
-    var deviceName = h('div', {}, 'Device: ' + (state.device || {}).name);
-    var ctl = h(
+    return h(
         'div',
         {class: 'row my-2'},
         h('div', {class: 'col-5 pr-1'},
@@ -156,10 +158,10 @@ var BLE = createClass({
             state.rpcParams)),
         h('div', {class: 'col-2 pl-1'},
           h('button', {
-            class: 'btn btn-block btn-sm btn-info',
+            class: 'btn btn-block btn-sm btn-warning',
             onClick: function(ev) {
               self.setState({rpcResult: '', spin: true, error: false});
-              blerpc(state.device, state.selectedMethod, state.rpcParams)
+              blerpc(app.state.device, state.selectedMethod, state.rpcParams)
                   .then(function(f) {
                     var res = f.error || f.result;
                     self.setState({
@@ -181,22 +183,117 @@ var BLE = createClass({
           h('pre', {class: 'my-2 p-2 ' + (state.error ? 'text-danger' : '')},
             state.rpcResult)),
     );
-    return h(
-        'div', {class: ''}, button,
-        h('div', {class: state.device ? '' : 'xd-none'}, deviceName, ctl));
   },
 });
 
+var WiFi = createClass({
+  init: function() {
+    this.state = {
+      ssid: '',
+      pass: '',
+      spin: false,
+    };
+  },
+  render: function(props, state) {
+    var self = this;
+    return h(
+        'div',
+        {class: 'form my-2'},
+        h('input', {
+          type: 'text',
+          name: 'wifi_ssid',
+          placeholder: 'WiFi network...',
+          onInput: function(ev) {
+            self.setState({ssid: ev.target.value});
+          },
+          class: 'form-control form-control-sm my-2'
+        },
+          state.ssid),
+        h('input', {
+          type: 'password',
+          name: 'wifi_pass',
+          placeholder: 'WiFi password...',
+          onInput: function(ev) {
+            self.setState({pass: ev.target.value});
+          },
+          class: 'form-control form-control-sm my-2'
+        },
+          state.pass),
+        h('button', {
+          class: 'btn btn-block btn-sm btn-warning',
+          onClick: function(ev) {
+            self.setState({spin: true});
+            var params = {
+              config: {
+                wifi: {
+                  ap: {enable: false},
+                  sta: {
+                    enable: true,
+                    ssid: self.state.ssid,
+                    pass: self.state.pass,
+                  }
+                }
+              }
+            };
+            blerpc(app.state.device, 'Config.Set', params)
+                .then(function(f) {
+                  blerpc(app.state.device, 'Config.Save', {reboot: true})
+                })
+                .catch(function(err) {
+                  alert(err);
+                })
+                .then(function(res) {
+                  self.setState({spin: false});
+                });
+          },
+          disabled: state.spin || !state.ssid || !app.state.device
+        },
+          mkicon(state.spin ? 'fa-refresh fa-spin' : 'fa-save'),
+          'Save WiFi settings'),
+    );
+  },
+});
+
+var Chooser = function(props) {
+  if (!navigator.bluetooth) {
+    return h(
+        'div', {class: 'my-2'},
+        h('div', {class: 'alert alert-danger'},
+          'BLE is not supported by this platform. Please use Chrome.'));
+  }
+  var button =
+      h('button', {
+        class: 'btn btn-sm btn-info btn-block my-2',
+        onClick: function() {
+          navigator.bluetooth
+              .requestDevice(
+                  {acceptAllDevices: true, optionalServices: [rpc_uuid]})
+              .then(function(dev) {
+                app.setState({device: dev});
+              })
+              .catch(function(err) {
+                alert(err);
+              });
+        },
+      },
+        mkicon('fa-search'), 'Choose device');
+  var name = app.state.device ? app.state.device.name : '<not chosen yet>'
+  var deviceName = h('div', {}, 'Device: ' + name);
+  return h('div', {class: ''}, button, deviceName);
+};
+
 var App = createClass({
   init: function(props) {
-    this.state = {};
+    this.state = {device: null};
+    app = this;
   },
   componentDidMount: function() {},
   render: function(props, state) {
     return h(
-        'div', {class: 'page h-100 container-fluid'},
+        'div', {class: 'page h-100 container-fluid'}, h(Header, props),
+        h(Chooser, props),
         h(preactRouter.Router, {history: History.createHashHistory()},
-          h('div', {default: true}, h(Header, props), h(BLE, props))));
+          h(RPC, {path: '/rpc'}), h(WiFi, {default: true})));
   },
 });
 
